af34ee7aae12c20b1e00db75343cc69da6931024
Lucas Manker <lucas.manker@horrocks.com>
2023-08-17 17:16:50 -0600

    deploy

--- src/routes/dynamicRouter.ts
+++ src/routes/dynamicRouter.ts
@@ -4,18 +4,31 @@ import getClientInfo from "../lib/api/getClientInfo";
 const dynamicRouter = express.Router();
 
 dynamicRouter.get("/:client", async (req: Request, res: Response) => {
+  console.log(req.originalUrl);
+  console.log("here", req.originalUrl.charAt(req.originalUrl.length - 1));
   const clientData = await getClientInfo(req.params.client);
   if (!clientData) {
     res.sendStatus(404);
   } else {
-    res.redirect(
-      `https://portal.horrocks.com/sites/informedStreetsDev/informedstreets/login/?client=${clientData.data.orgUrl}`
-    );
+    try {
+      req.originalUrl.charAt(req.originalUrl.length - 1) === "/"
+        ? res.redirect(`../login/?client=${clientData.data.orgUrl}`)
+        : res.redirect(`./login/?client=${clientData.data.orgUrl}`);
+    } catch {
+      res.sendStatus(404);
+    }
   }
 });
 
-dynamicRouter.get("/client/:project", (req: Request, res: Response) => {
-  res.redirect(`/?client=${req.params.project}`);
+dynamicRouter.get("/:client/:project", async (req: Request, res: Response) => {
+  const clientData = await getClientInfo(req.params.client);
+  try {
+    res.redirect(
+      `../login/?client=${clientData.data.orgUrl}&projectIndex=${req.params.project}`
+    );
+  } catch {
+    res.sendStatus(404);
+  }
 });
 
 export default dynamicRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import dynamicRouter from "./routes/dynamicRouter";
-import path from "path";
 
 const app: Application = express();
 
-app.use("/", express.static(path.join(__dirname, "../testing")));
+app.use("/", express.static("C:/inetpub/wwwroot/informedstreets/"));
+//app.use("/", express.static("E:/websites/InformedStreetsDev/InformedStreets/"));
 app.use("/", dynamicRouter);
 
 app.listen(49012, (): void => {
